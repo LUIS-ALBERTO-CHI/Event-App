1bf6531f51b1f50bfa051c10cbf3bc9e11a79aca
Luis A. Casanova <132479713+LuisCasanova11@users.noreply.github.com>
2024-07-13 20:00:26 -0600

    s
    s

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,121 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <HeaderComponent :isLoggedIn="isLoggedIn" :username="username" @logout="handleLogout" />
+    <router-view @login-success="handleLogin"></router-view>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponent from './components/HeaderComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderComponent
+  },
+  data() {
+    return {
+      isLoggedIn: false,
+      username: ''
+    };
+  },
+  methods: {
+    handleLogin(token) {
+      console.log('User logged in', token);
+
+      if (!token) {
+        console.error('Invalid token received:', token);
+        return;
+      }
+
+      try {
+        const payloadBase64 = token.split('.')[1];
+        const decodedPayload = JSON.parse(atob(payloadBase64));
+        console.log('Decoded token:', decodedPayload);
+
+        localStorage.setItem('accessToken', token);
+
+        this.isLoggedIn = true;
+        this.username = decodedPayload.name;
+      } catch (error) {
+        console.error('Error decoding or storing token:', error);
+      }
+    },
+    handleLogout() {
+      localStorage.removeItem('accessToken');
+      this.isLoggedIn = false;
+      this.username = '';
+    },
+    checkAuth() {
+      const token = localStorage.getItem('accessToken');
+      if (token) {
+        try {
+          const payloadBase64 = token.split('.')[1];
+          const decodedPayload = JSON.parse(atob(payloadBase64));
+          this.isLoggedIn = true;
+          this.username = decodedPayload.name;
+        } catch (error) {
+          console.error('Invalid token', error);
+          this.isLoggedIn = false;
+          this.username = '';
+        }
+      }
+    }
+  },
+  created() {
+    this.checkAuth();
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: SF Pro Text, Helvetica Neue, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 14px;
+  height: 100%;
+  background: linear-gradient(to bottom right, #007AFF, #00A3E3);
+  position: relative;
+  overflow: hidden;
+}
+
+#app::before, #app::after {
+  content: "";
+  position: absolute;
+  border-radius: 50%;
+  background: rgba(255, 255, 255, 0.15);
+  opacity: 0.5;
+}
+
+#app::before {
+  width: 200px;
+  height: 200px;
+  top: 20%;
+  left: 10%;
+}
+
+#app::after {
+  width: 300px;
+  height: 300px;
+  bottom: 15%;
+  right: 20%;
+}
+
+@media (max-width: 768px) {
+  #app::before {
+    width: 150px;
+    height: 150px;
+    top: 25%;
+    left: 15%;
+  }
+
+  #app::after {
+    width: 200px;
+    height: 200px;
+    bottom: 20%;
+    right: 25%;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/lara-light-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+import HomePage from './components/HomePage.vue';
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/', component: HomePage }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+const app = createApp(App);
+app.use(PrimeVue);
+app.use(router); 
+
+app.mount('#app');
